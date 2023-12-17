2858358e2ec8f8c0dacc3645dbf27361d5063872
Puru-Rana <pururana24@gmail.com>
2023-12-17 18:06:55 -0500

    dark ? light : dark

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Link from "next/link";
+import ThemeSwitch from "./ThemeSwitch";
+
+const Navbar = () => {
+  return (
+    <div className="mx-auto max-w-5xl px-6">
+      <div className="flex justify-between items-center h-16 w-full">
+        <Link href="/">
+          <div>Guru Blog</div>
+        </Link>
+        <ThemeSwitch />
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
