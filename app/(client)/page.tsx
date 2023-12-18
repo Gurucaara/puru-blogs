51df5640a323d99fc091fa601c004bb5e15d8a27
Puru-Rana <pururana24@gmail.com>
2023-12-18 18:52:53 -0500

    Get Posts From CMS

--- app/(client)/page.tsx
+++ app/(client)/page.tsx
@@ -1,7 +1,30 @@
-export default function Home() {
+import { client } from "@/sanity/lib/client";
+import Header from "../components/Header";
+import { Post } from "../utils/interface";
+import PostComponent from "../components/PostComponent";
+
+async function getPosts() {
+  const query = `
+    *[_type == "post"] {
+      title,
+      slug,
+      publishedAt,
+      excerpt,
+    } `;
+  const data = await client.fetch(query);
+  return data;
+}
+
+export default async function Home() {
+  const posts: Post[] = await getPosts();
+  console.log(posts, "posts");
   return (
     <div>
-      <h1>Hello World!</h1>
+      <Header title="Articles" />
+      <div>
+        {posts?.length > 0 &&
+          posts?.map((post) => <PostComponent key={post?._id} post={post} />)}
+      </div>
     </div>
   );
 }
