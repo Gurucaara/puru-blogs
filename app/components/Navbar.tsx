56a662c9eab4668526ef82df111cde45a0166a9a
Puru-Rana <pururana24@gmail.com>
2023-12-17 19:18:26 -0500

    Logo Setup

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,13 +1,19 @@
 import React from "react";
 import Link from "next/link";
 import ThemeSwitch from "./ThemeSwitch";
+import { Lilita_One } from "next/font/google";
+
+const font = Lilita_One({ weight: "400", subsets: ["latin"] });
 
 const Navbar = () => {
   return (
     <div className="mx-auto max-w-5xl px-6">
       <div className="flex justify-between items-center h-16 w-full">
         <Link href="/">
-          <div>Guru Blog</div>
+          <div className={`${font.className} text-3xl dark:text-amber-50`}>
+            Guru
+            <span className="text-purple-500">Blog</span>
+          </div>
         </Link>
         <ThemeSwitch />
       </div>
